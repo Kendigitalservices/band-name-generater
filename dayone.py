0dd2612e3f18bfcd19ab11f7428d243d27416869
Kendigitalservices <kendigitalworks@gmail.com>
2025-05-19 22:48:56 +0300

    Update and rename README.md to dayone.py

---
+++ dayone.py
@@ -0,0 +1,9 @@
+print("Welcome to a band name generator.")
+
+city = input("What's the name of the city you were born in?\n ")
+
+pet = input("What's your pet's name? \n")
+
+band_name = city +" "+ pet 
+
+print(f"Your band name could be {band_name}")
